93d788fd8a5da4085cd5b3eadc54467cdde080cd
zhuhuren <zhuqingmo@hotmail.com>
2017-01-19 14:48:03 -0700

    drawTrees1.py added

--- drawTrees1.py
+++ drawTrees1.py
@@ -8,20 +8,23 @@ def tree(lineLen, t):
         angle2 = randint(5, 45)
         lenDecrease = randint(10, 15)
         t.color('black')
-        pen = lineLen // 8
-        if pen == 0: pen = 1
+        pen = 1
+##        pen = lineLen // 8
+##        if pen == 0: pen = 1
         if lineLen - lenDecrease <= 5:
             t.color('green')
             pen = 5
         t.pensize(pen)
-        t.down()
+##        t.down()
         t.forward(lineLen)
         t.right(angle1)
         tree(lineLen-lenDecrease, t)
         t.left(angle1 + angle2)
         tree(lineLen-lenDecrease, t)
-        t.right(angle2)
-        t.up()
+        t.right(angle2 + 0.01)
+##        t.up()
+        t.color('black')
+        t.pensize(1)
         t.backward(lineLen)
 
 if __name__ == '__main__':
